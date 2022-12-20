8f9897992908b9dd9a27f2a529b245b9e463fbad
Jamal Jones <74991730+OfficialJamalJones@users.noreply.github.com>
2022-12-20 13:05:00 -0500

    Add files via upload

---
+++ Networking_Jamal_Jones/Networking_Jamal_Jones/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Networking_Jamal_Jones
+//
+//  Created by Consultant on 12/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
